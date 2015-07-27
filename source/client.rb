c3befb249e786977f09415c03c890883c276c2d2
Blanche Hollingshead & Nicholas Kincaid <pair+alita42.nbkincaid@devbootcamp.com>
2015-07-27 14:59:46 -0500

    adding HTTP client/server solutions

--- source/client.rb
+++ source/client.rb
@@ -1,1 +1,18 @@
 require 'socket'
+
+msg = "GET /index.html HTTP/1.1\nHost: www.example.com\n\n"
+host = "www.example.com"
+port = 80
+
+server_connection = TCPSocket.new(host, port)
+server_connection.print(msg)
+
+while server_connection.gets != ""
+  if server_connection.gets.include?"</html>"
+    puts server_connection.gets
+    break
+  else
+    puts server_connection.gets
+  end
+end
+
--- source/server.rb
+++ source/server.rb
@@ -1,1 +1,36 @@
 require 'socket'
+
+
+server = TCPServer.new(2000)
+
+loop do
+
+  client = server.accept
+
+  request = client.gets
+  resource = request.split(" ")[1]
+
+  if resource == "/hi"
+    title = "Greetings"
+    msg =  "Yar Matey!"
+  elsif resource == "/quote"
+    title = "Quotes"
+    msg = "Cowards die many times before their death, the valiant never taste of death but once."
+  else
+    title = "An Example Page"
+    msg = "Hello World, this is a very simple HTML document."
+  end
+
+  html_rendering = "<html><head><title>#{title}</title></head><body>#{msg}</body></html>"
+
+
+  client.puts "HTTP/1.1 200 OK"
+  client.puts "Content-Length: #{html_rendering.length}"
+  client.puts ""
+  client.puts html_rendering
+
+
+  client.close
+
+
+end
