39dc3f7ccd6610a9cce8ed8fdec6b555729a514e
Connor Ring & ksliu25 <pair+ccring.ksliu25@devbootcamp.com>
2015-07-27 14:24:17 -0500

    HTTP

--- source/client.rb
+++ source/client.rb
@@ -1,1 +1,10 @@
 require 'socket'
+server_connection = TCPSocket.new('10.1.10.131', 2000)
+
+server_connection.puts("GET /index.html HTTP/1.1")
+server_connection.puts("Host: 10.1.10.131")
+server_connection.puts("")
+
+until (line = server_connection.gets).nil?
+  puts line
+end
--- source/server.rb
+++ source/server.rb
@@ -1,1 +1,45 @@
 require 'socket'
+server = TCPServer.new(2000)
+
+loop do
+
+  client = server.accept
+  puts "A client has connected"
+  p input = client.gets.chomp
+  # p input2 = client.gets
+  # p input3 = client.gets
+
+  if input == "GET /index.html HTTP/1.1" #&& input2 == "Host: 10.1.10.131" && input3 == ""
+    client.puts("HTTP/1.1 200 OK")
+    client.puts("Content-Length: 130")
+    client.puts("")
+    client.puts("<html>")
+    client.puts("<head>")
+    client.puts("  <title>An Example Page</title>")
+    client.puts("</head>")
+    client.puts("<body>")
+    client.puts("  Hello World, this is a very simple HTML document.")
+    client.puts("</body>")
+    client.puts("</html>")
+  elsif input == "GET /hi HTTP/1.1" #&& input2 == "Host: 10.1.10.131" && input3 == ""
+    content = "<html><head><title>An Example Page</title></head><body>Hi.</body></html>"
+    client.puts("HTTP/1.1 200 OK")
+    client.puts("Content-Length: #{content.length}")
+    client.puts("")
+    client.puts(content)
+  elsif input == "GET /quote HTTP/1.1" #&& input2 == "Host: 10.1.10.131" && input3 == ""
+    content = "<html><head><title>An Example Page</title></head><body>quote.</body></html>"
+    client.puts("HTTP/1.1 200 OK")
+    client.puts("Content-Length: #{content.length}")
+    client.puts("")
+    client.puts(content)
+  else
+    client.puts("HTTP/1.1 404 NOT FOUND")
+  end
+
+  client.close
+
+
+end
+
+
