ccc807c97cf61b381bbe4e248b34e0d6a07f2215
Josh Jones & Matt Bogen <pair+z3ntech.zebogen@devbootcamp.com>
2015-07-27 10:42:22 -0500

    do release 1 correctly

--- source/client.rb
+++ source/client.rb
@@ -1,7 +1,11 @@
 require 'socket'
 
-Socket.tcp("www.example.com", 80) do |socket|
-  socket.print "GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n"
-  socket.close_write
-  puts socket.read
+server_connection = TCPSocket.new('www.example.com', 80)
+
+server_connection.puts("GET / HTTP/1.1\nHost: www.example.com\n\n")
+
+response_line = server_connection.gets
+until response_line == nil
+  puts response_line
+  response_line = server_connection.gets
 end
