d108ed183c5b4ad018be386a5daf20d5c0089d78
Jay Quigley & MatthewGreenberg & Spencer Rohan <pair+jayqui.matthewgreenberg.spencerrohan@devbootcamp.com>
2015-07-27 14:30:29 -0500

    complete challenge

--- source/client.rb
+++ source/client.rb
@@ -1,1 +1,26 @@
 require 'socket'
+
+url = "GET /index.html HTTP/1.1
+Host: example.com"
+
+greeting = "GET /hi HTTP/1.1
+Host: example.com"
+
+quote = "GET /quote HTTP/1.1
+Host: example.com"
+# host = ""
+
+
+server_connection = TCPSocket.new('www.example.com', 80)
+
+server_connection.puts url
+# server_connection.puts host
+server_connection.puts
+
+until (line = server_connection.gets).nil?
+  puts line
+end
+
+
+
+
--- source/server.rb
+++ source/server.rb
@@ -1,1 +1,56 @@
 require 'socket'
+
+server = TCPServer.new(2000)
+# greetingserv = TCPServer.new(2000/hi)
+# quoteserv = TCPServer.new(2000/quote)
+
+
+
+loop do
+  client = server.accept
+  path = client.gets.chomp
+  p path
+
+  if path == "GET / HTTP/1.1"
+    puts "client has connected"
+    client.puts "HTTP/1.1 200 OK"
+    client.puts "Content-Length: 130"
+    client.puts
+
+    client.puts "<html>"
+    client.puts "<head>"
+    client.puts "  <title>An Example Page</title>"
+    client.puts "</head>"
+    client.puts "<body>"
+    client.puts "  Hello World, this is a very simple HTML document."
+    client.puts "</body>"
+    client.puts "</html>"
+
+  elsif path == "GET /hi HTTP/1.1"
+    puts "client has connected"
+    client.puts "HTTP/1.1 200 OK"
+    client.puts
+    client.puts "<html>"
+    client.puts "<head>"
+    client.puts "  <title>An Example Page</title>"
+    client.puts "</head>"
+    client.puts "<body>"
+    client.puts "  Welcome to the party pallllll!!"
+    client.puts "</body>"
+    client.puts "</html>"
+  elsif path == "GET /quote HTTP/1.1"
+    puts "client has connected"
+    client.puts "HTTP/1.1 200 OK"
+    client.puts
+    client.puts "<html>"
+    client.puts "<head>"
+    client.puts "  <title>An Example Page</title>"
+    client.puts "</head>"
+    client.puts "<body>"
+    client.puts "  To air is human"
+    client.puts "</body>"
+    client.puts "</html>"
+  end
+
+  client.close
+end
