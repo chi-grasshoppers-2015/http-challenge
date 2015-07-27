5b2a78e5831c376670aa90dd31a5be76bf2e0569
Josh Jones & Matt Bogen <pair+z3ntech.zebogen@devbootcamp.com>
2015-07-27 11:03:28 -0500

    Complete release 2

--- source/server.rb
+++ source/server.rb
@@ -1,1 +1,21 @@
 require 'socket'
+
+server = TCPServer.new(2000)
+
+loop do
+
+  client = server.accept
+
+  puts "Client connected"
+  request = client.gets
+  puts "Received #{request}"
+
+  response = "HTTP/1.1 200 OK\nContent-Length: 127\n\n<html>\n<head>\n<title>An Example Page</title>\n</head>\n<body>\n  Hello World, this is a very simple HTML document.\n</body>\n</html>"
+
+  puts "Sending #{response}"
+
+  client.puts response
+
+  client.close
+
+end
