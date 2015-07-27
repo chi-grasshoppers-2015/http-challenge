a98960c1e90bb44026e26e5e734f48c940b19cca
Josh Jones & Matt Bogen <pair+z3ntech.zebogen@devbootcamp.com>
2015-07-27 11:19:01 -0500

    Complete challenge 2 -- HTTP challenge

--- source/server.rb
+++ source/server.rb
@@ -7,10 +7,22 @@ loop do
   client = server.accept
 
   puts "Client connected"
-  request = client.gets
+  request = client.gets.chomp
   puts "Received #{request}"
 
-  response = "HTTP/1.1 200 OK\nContent-Length: 127\n\n<html>\n<head>\n<title>An Example Page</title>\n</head>\n<body>\n  Hello World, this is a very simple HTML document.\n</body>\n</html>"
+  base_content_length = 78
+  if request == "GET /hi HTTP/1.1"
+    message = "Welcome to my tiny web page."
+  elsif request == "GET /quote HTTP/1.1"
+    message = "Do or do not, there is not try."
+  else
+    puts "Bad request REJECTED!"
+    client.puts "HTTP/1.1 404 Not Found\nContent-Length: 95\n\n<html>\n<head>\n<title>404 Not Found</title>\n</head>\n<body>\nBad request REJECTED!\n</body>\n</html>"
+    client.close
+    next
+  end
+
+  response = "HTTP/1.1 200 OK\nContent-Length: #{base_content_length + message.length}\n\n<html>\n<head>\n<title>An Example Page</title>\n</head>\n<body>\n  #{message}\n</body>\n</html>"
 
   puts "Sending #{response}"
 
