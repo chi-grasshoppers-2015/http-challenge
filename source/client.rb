72a8fcc52843c133a10e16d13edef59746e1e351
Jasmine Feldmann & linas1985 <pair+jasmine-feldmann.linas1985@devbootcamp.com>
2015-07-28 12:54:18 -0500

    Fix to acutally write HTML to page

--- source/client.rb
+++ source/client.rb
@@ -1,1 +1,15 @@
 require 'socket'
+
+
+server_connection = TCPSocket.new('www.example.com', 80)
+
+
+server_connection.puts "GET /index.html HTTP/1.1"
+server_connection.puts "Host: example.com"
+server_connection.puts ""
+
+until server_connection.gets == nil
+  puts server_connection.gets
+end
+
+
